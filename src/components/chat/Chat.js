6328cbdaaf556497b6eae2a209a55a2eff2b3a4b
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-07-07 01:47:04 +0530

    Creating the basic layout

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './chat.css'
+import EmojiPicker from 'emoji-picker-react'
 
 const Chat = () => {
+
+    const [open,setOpen]=useState(false)
+    const [text,setText]=useState('')
+
+    const handleEmoji=(e)=>{
+        setText(prev=>prev+e.emoji)
+        setOpen(false)
+    }
+
   return (
     <div className='chat'>
       <div className='top'>
@@ -18,8 +28,70 @@ const Chat = () => {
             <img src='/info.png' alt=''/>
         </div>
       </div>
-      <div className='center'></div>
-      <div className='bottom'></div>
+      <div className='center'>
+        <div className='message own'>
+            <div className='texts'>
+                <p>Lorem ipsum sic mundus dark series est</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className='message'>
+            <img src='/avatar.png' alt=''/>
+            <div className='texts'>
+                <p>Lorem ipsum sic mundus dark series est</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className='message own'>
+            <div className='texts'>
+                <p>Lorem ipsum sic mundus dark series est</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className='message'>
+            <img src='/avatar.png' alt=''/>
+            <div className='texts'>
+                <p>Lorem ipsum sic mundus dark series est</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className='message'>
+            <img src='/avatar.png' alt=''/>
+            <div className='texts'>
+                <p>Lorem ipsum sic mundus dark series est</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className='message'>
+            <img src='/avatar.png' alt=''/>
+            <div className='texts'>
+                <p>Lorem ipsum sic mundus dark series est</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+        <div className='message own'>
+            <div className='texts'>
+                <img src='https://images.nightcafe.studio/jobs/PO9BaVxqMqce5cq3ju0D/PO9BaVxqMqce5cq3ju0D--1--jsjn1.jpg?tr=w-1600,c-at_max' alt=''/>
+                <p>Lorem ipsum sic mundus dark series est</p>
+                <span>1 min ago</span>
+            </div>
+        </div>
+      </div>
+      <div className='bottom'>
+        <div className='icons'>
+            <img src='/img.png' alt=''/>
+            <img src='/camera.png' alt=''/>
+            <img src='/mic.png' alt=''/>
+        </div>
+        <input type='text' placeholder='Type a message' value={text} onChange={e=>setText(e.target.value)}/>
+        <div className='emoji'>
+            <img src='/emoji.png' alt='' onClick={()=>setOpen(!open)}/>
+            <div className='picker'>
+                <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+            </div>
+        </div>
+        <button className='send'>Send</button>
+      </div>
     </div>
   )
 }
--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -4,7 +4,83 @@ import './detail.css'
 const Detail = () => {
   return (
     <div className='detail'>
-      Detail
+      <div className='user'>
+        <img src='/avatar.png' alt=''/>
+        <h2>Jack Sparrow</h2>
+        <p>Lorem ipsim salt is tasti</p>
+      </div>
+      <div className='info'>
+        <div className='option'>
+            <div className='title'>
+                <span>Chat settings</span>
+                <img src='/arrowUp.png' alt=''/>
+            </div>
+        </div>
+        <div className='option'>
+            <div className='title'>
+                <span>Privacy</span>
+                <img src='/arrowUp.png' alt=''/>
+            </div>
+        </div>
+        <div className='option'>
+            <div className='title'>
+                <span>Shared photos</span>
+                <img src='/arrowDown.png' alt=''/>
+            </div>
+            <div className='photos'>
+                    <div className='photoItem'>
+                        <div className='photoDetail'>
+                            <img src='https://images.nightcafe.studio/jobs/PO9BaVxqMqce5cq3ju0D/PO9BaVxqMqce5cq3ju0D--1--jsjn1.jpg?tr=w-1600,c-at_max' alt=''/>
+                            <span>selfie_2024.png</span>
+                        </div>
+                    </div>
+                    <img src='/download.png' alt=''/>
+            </div>
+            <div className='photos'>
+                    <div className='photoItem'>
+                        <div className='photoDetail'>
+                            <img src='https://images.nightcafe.studio/jobs/PO9BaVxqMqce5cq3ju0D/PO9BaVxqMqce5cq3ju0D--1--jsjn1.jpg?tr=w-1600,c-at_max' alt=''/>
+                            <span>selfie_2024.png</span>
+                        </div>
+                    </div>
+                    <img src='/download.png' alt=''/>
+            </div>
+            <div className='photos'>
+                    <div className='photoItem'>
+                        <div className='photoDetail'>
+                            <img src='https://images.nightcafe.studio/jobs/PO9BaVxqMqce5cq3ju0D/PO9BaVxqMqce5cq3ju0D--1--jsjn1.jpg?tr=w-1600,c-at_max' alt=''/>
+                            <span>selfie_2024.png</span>
+                        </div>
+                    </div>
+                    <img src='/download.png' alt=''/>
+            </div>
+            <div className='photos'>
+                    <div className='photoItem'>
+                        <div className='photoDetail'>
+                            <img src='https://images.nightcafe.studio/jobs/PO9BaVxqMqce5cq3ju0D/PO9BaVxqMqce5cq3ju0D--1--jsjn1.jpg?tr=w-1600,c-at_max' alt=''/>
+                            <span>selfie_2024.png</span>
+                        </div>
+                    </div>
+                    <img src='/download.png' alt=''/>
+            </div>
+            <div className='photos'>
+                    <div className='photoItem'>
+                        <div className='photoDetail'>
+                            <img src='https://images.nightcafe.studio/jobs/PO9BaVxqMqce5cq3ju0D/PO9BaVxqMqce5cq3ju0D--1--jsjn1.jpg?tr=w-1600,c-at_max' alt=''/>
+                            <span>selfie_2024.png</span>
+                        </div>
+                    </div>
+                    <img src='/download.png' alt=''/>
+            </div>
+        </div>
+        <div className='option'>
+            <div className='title'>
+                <span>Shared files</span>
+                <img src='/arrowUp.png' alt=''/>
+            </div>
+        </div>
+        <button>Block user</button>
+      </div>
     </div>
   )
 }
